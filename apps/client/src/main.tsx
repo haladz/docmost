e3185fb9a463940aec85995d0956d8f18ae56be9
haladz <andalusy.2000@gmail.com>
2025-05-20 16:53:13 +0300

    feat(i18n): begin arabic translations

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,14 +3,16 @@ import "@mantine/spotlight/styles.css";
 import "@mantine/notifications/styles.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { mantineCssResolver, theme } from '@/theme';
+import { mantineCssResolver, theme } from "@/theme";
 import { MantineProvider } from "@mantine/core";
 import { BrowserRouter } from "react-router-dom";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
-import "./i18n";
+import i18n from "./i18n";
+import { isRtl } from "@/lib/rtl-langs";
+import { useEffect, useState } from "react";
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -24,21 +26,43 @@ export const queryClient = new QueryClient({
 });
 
 
+function Root() {
+  const [dir, setDir] = useState(isRtl(i18n.language) ? "rtl" : "ltr");
+
+  useEffect(() => {
+    const handler = (lng: string) => {
+      const newDir = isRtl(lng) ? "rtl" : "ltr";
+      document.documentElement.dir = newDir;
+      setDir(newDir);
+    };
+    i18n.on("languageChanged", handler);
+    handler(i18n.language);
+    return () => {
+      i18n.off("languageChanged", handler);
+    };
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <MantineProvider
+        theme={{ ...theme, dir }}
+        cssVariablesResolver={mantineCssResolver}
+      >
+        <ModalsProvider>
+          <QueryClientProvider client={queryClient}>
+            <Notifications position="bottom-center" limit={3} />
+            <HelmetProvider>
+              <App />
+            </HelmetProvider>
+          </QueryClientProvider>
+        </ModalsProvider>
+      </MantineProvider>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
-root.render(
-  <BrowserRouter>
-    <MantineProvider theme={theme} cssVariablesResolver={mantineCssResolver}>
-      <ModalsProvider>
-        <QueryClientProvider client={queryClient}>
-          <Notifications position="bottom-center" limit={3} />
-          <HelmetProvider>
-            <App />
-          </HelmetProvider>
-        </QueryClientProvider>
-      </ModalsProvider>
-    </MantineProvider>
-  </BrowserRouter>
-);
+root.render(<Root />);
